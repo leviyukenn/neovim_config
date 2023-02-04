c66b4f8d745440ffda66e48e953f816c45ef36fe
Cyno <cyno@timetreeapp.com>
2023-02-04 20:54:12 +0900

    :sparkles: update dashboard-nvim

--- lua/user/plugins-config/dashboard.lua
+++ lua/user/plugins-config/dashboard.lua
@@ -4,62 +4,42 @@ if not status then
 	return
 end
 
-db.custom_center = {
-	{
-		icon = "  ",
-		desc = "Projects                            ",
-		action = "Telescope projects",
+db.setup({
+	theme = "doom",
+	config = {
+		header = {
+			[[]],
+			[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+			[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+			[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+			[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+			[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+			[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+			[[                                                   ]],
+			[[                [ version : 1.0.0 ]                ]],
+		},
+		center = {
+			{
+				icon = "  ",
+				desc = "Projects                            ",
+				action = "Telescope projects",
+			},
+			{
+				icon = "  ",
+				desc = "Recently files                      ",
+				action = "Telescope oldfiles",
+			},
+			{
+				icon = "  ",
+				desc = "Edit keybindings                    ",
+				action = "edit ~/.config/nvim/lua/user/keymaps.lua",
+			},
+			{
+				icon = "  ",
+				desc = "Edit Projects                       ",
+				action = "edit ~/.local/share/nvim/project_nvim/project_history",
+			},
+		},
+		footer = {},
 	},
-	{
-		icon = "  ",
-		desc = "Recently files                      ",
-		action = "Telescope oldfiles",
-	},
-	{
-		icon = "  ",
-		desc = "Edit keybindings                    ",
-		action = "edit ~/.config/nvim/lua/user/keymaps.lua",
-	},
-	{
-		icon = "  ",
-		desc = "Edit Projects                       ",
-		action = "edit ~/.local/share/nvim/project_nvim/project_history",
-	},
-	-- {
-	--   icon = "  ",
-	--   desc = "Edit .bashrc                        ",
-	--   action = "edit ~/.bashrc",
-	-- },
-	-- {
-	--   icon = "  ",
-	--   desc = "Change colorscheme                  ",
-	--   action = "ChangeColorScheme",
-	-- },
-	-- {
-	--   icon = "  ",
-	--   desc = "Edit init.lua                       ",
-	--   action = "edit ~/.config/nvim/init.lua",
-	-- },
-	-- {
-	--   icon = "  ",
-	--   desc = "Find file                           ",
-	--   action = "Telescope find_files",
-	-- },
-	-- {
-	--   icon = "  ",
-	--   desc = "Find text                           ",
-	--   action = "Telescopecope live_grep",
-	-- },
-}
-
-db.custom_header = {
-	[[]],
-	[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
-	[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
-	[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
-	[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
-	[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
-	[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-	[[                                                   ]],
-	[[                [ version : 1.0.0 ]                ]],
-}
+})
